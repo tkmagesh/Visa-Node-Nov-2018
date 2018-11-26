e5fad0962904af35503acb7a513d07f1b86d5b02
Magesh <tkmagesh77@gmail.com>
2018-11-26 16:16:07 +0000

    webserver implementation

---
+++ 05-webServer/server.js
@@ -0,0 +1,23 @@
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
+
+var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
+	console.log(req.method + '\t' + req.url);
+	var resourcePath = path.join(__dirname, req.url);
+	var stream = fs.createReadStream(resourcePath);
+	stream.on('data', function(chunk){
+		res.write(chunk);
+	});
+	stream.on('end', function(){
+		res.end();
+	});
+	stream.on('error', function(err){
+		res.write(err.message);
+		res.end();
+	});
+});
+
+server.listen(8080);
+
+console.log('server listening on 8080!');
