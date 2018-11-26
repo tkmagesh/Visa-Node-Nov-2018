4d1119011b244d32249cdf92b616e00f2ba34779
Magesh <tkmagesh77@gmail.com>
2018-11-26 16:21:42 +0000

    handling 404

--- 05-webServer/server.js
+++ 05-webServer/server.js
@@ -5,13 +5,19 @@ var http = require('http'),
 var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
 	console.log(req.method + '\t' + req.url);
 	var resourcePath = path.join(__dirname, req.url);
+	if (!fs.existsSync(resourcePath)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
 	var stream = fs.createReadStream(resourcePath);
-	stream.on('data', function(chunk){
+	/*stream.on('data', function(chunk){
 		res.write(chunk);
 	});
 	stream.on('end', function(){
 		res.end();
-	});
+	});*/
+	stream.pipe(res);
 	stream.on('error', function(err){
 		res.write(err.message);
 		res.end();
