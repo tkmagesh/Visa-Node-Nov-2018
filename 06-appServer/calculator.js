54148af58f0c607d8b5594ff04f5fc86b5d5d305
Magesh <tkmagesh77@gmail.com>
2018-11-26 16:36:16 +0000

    app server for calculator

---
+++ 06-appServer/calculator.js
@@ -0,0 +1,27 @@
+var calculator = {
+	add : function(x,y){
+		return x + y;
+	},
+	subtract : function(x,y){
+		return x - y;
+	},
+	multiply : function(x,y){
+		return x * y;
+	},
+	divide : function(x,y){
+		if (y === 0)
+			throw new Error('Invalid arguments');
+		return x / y;
+	},
+	divideAsync : function(x,y, resultCallback){
+		setTimeout(function(){
+			if (y === 0){
+				var e = new Error('Invalid arguments');
+				return resultCallback(e);
+			}	
+			var result = x / y;
+			return resultCallback(null, result);
+		}, 3000);
+	}
+};
+module.exports = calculator;
---
+++ 06-appServer/server.js
@@ -0,0 +1,30 @@
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
+	url = require('url'),
+	querystring = require('querystring'),
+	calculator = require('./calculator');
+
+var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
+	
+	var urlObj =  url.parse(req.url);
+	console.log(req.method + '\t' + urlObj.pathname);
+	if (urlObj.pathname !== '/calculator'){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	var queryData = querystring.parse(urlObj.query),
+		x = parseInt(queryData.x),
+		y = parseInt(queryData.y),
+		op = queryData.op;
+
+	var result = calculator[op](x,y);
+	res.write(result.toString());
+	res.end();
+
+});
+
+server.listen(8080);
+
+console.log('server listening on 8080!');
