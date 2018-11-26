680e4a1583e4f1e86acd70e97e14a49d008f0a34
Magesh <tkmagesh77@gmail.com>
2018-11-26 16:32:15 +0000

    web server complete

--- 05-webServer/server.js
+++ 05-webServer/server.js
@@ -1,10 +1,13 @@
 var http = require('http'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	url = require('url');
 
 var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
-	console.log(req.method + '\t' + req.url);
-	var resourcePath = path.join(__dirname, req.url);
+	
+	var urlObj =  url.parse(req.url);
+	console.log(req.method + '\t' + urlObj.pathname);
+	var resourcePath = path.join(__dirname, urlObj.pathname);
 	if (!fs.existsSync(resourcePath)){
 		res.statusCode = 404;
 		res.end();
