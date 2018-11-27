ba9c5db3c972c3556d5143fa1b2808e312396bf7
Magesh <tkmagesh77@gmail.com>
2018-11-27 15:41:52 +0000

    implementing put requests

--- 09-taskManager/routes/tasks.js
+++ 09-taskManager/routes/tasks.js
@@ -23,6 +23,22 @@ router.post('/', function(req, res, next){
 	res.status(201).json(newTask);
 });
 
+router.put('/:id', function(req, res, next){
+	var taskIdToUpdate = parseInt(req.params.id);
+	var updatedTask = req.body;
+	var taskToUpdate = tasks.find(function(task){
+		return task.id === taskIdToUpdate;
+	});
+	if (taskToUpdate){
+		tasks = tasks.map(function(task){
+			return task.id === taskIdToUpdate ? updatedTask : task;
+		});
+		res.status(200).json(updatedTask);
+	} else {
+		res.status(404).end();
+	}
+});
+
 module.exports = router;
 
 /* http://localhost:3000/tasks */
