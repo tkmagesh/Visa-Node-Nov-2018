636497a3128c5c606c5a58e4675bb939feef3937
Magesh <tkmagesh77@gmail.com>
2018-11-27 11:22:19 +0000

    webappserver - refactored - solution - part 3 and assignment

--- 08-webappServer-refactored/serveStatic.js
+++ 08-webappServer-refactored/serveStatic.js
@@ -9,27 +9,12 @@ function isStatic(resource){
 }
 
 module.exports = function(req, res, next){
-	var resourceName = req.urlObj.pathname;
-	if (isStatic(resourceName)){
-		var resourcePath = path.join(__dirname, resourceName);
-		if (!fs.existsSync(resourcePath)){
-			console.log('[@serveStatic] - serving 404');
-			res.statusCode = 404;
-			res.end();
-			return;
-		}
+	var resourceName = req.urlObj.pathname,
+		resourcePath = path.join(__dirname, resourceName);
+	if (isStatic(resourceName) && fs.existsSync(resourcePath)){
 		var stream = fs.createReadStream(resourcePath);
 		stream.pipe(res);
 		stream.on('end', next);
-		/*stream.on('data', function(chunk){
-			console.log('[@serveStatic] - serving file chunk');
-			res.write(chunk);
-		});
-		stream.on('end', function(){
-			console.log('[@serveStatic] - closing the response stream');
-			res.end();
-			next();
-		});*/
 	} else {
 		next();
 	}
--- 08-webappServer-refactored/server.js
+++ 08-webappServer-refactored/server.js
@@ -1,33 +1,17 @@
 var http = require('http');
 
-var dataParser = require('./dataParser'),
+var app = require('./app'),
+	dataParser = require('./dataParser'),
 	serveStatic = require('./serveStatic'),
 	serveCalculator = require('./serveCalculator'),
-	notFoundHandler = require('./notFoundHandler')
+	notFoundHandler = require('./notFoundHandler');
 
-var _middlewares = [dataParser, serveStatic, serveCalculator, notFoundHandler];
 
-function exec(req, res, middlewares){
-	var first = middlewares[0],
-		remaining = middlewares.slice(1),
-		next = function(){
-			exec(req, res, remaining);
-		};
-	if (typeof first === 'function')
-		first(req, res, next);
-}
+app.use(dataParser); 
+app.use(serveStatic); 
+app.use(serveCalculator); 
+app.use(notFoundHandler);
 
-var server = http.createServer(function(req, res){
-	/*dataParser(req);
-	console.log(req.method + '\t' + req.urlObj.pathname);
-	serveStatic(req, res);
-	serveCalculator(req, res);
-	notFoundHandler(res);*/
-
-	exec(req, res, _middlewares);
-	
-});
-
-server.listen(8080);
+http.createServer(app).listen(8080);
 
 console.log('server listening on 8080!');
