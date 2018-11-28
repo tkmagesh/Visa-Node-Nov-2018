88c844190b0a4e0419cedc8b55389d3c4326633f
Magesh <tkmagesh77@gmail.com>
2018-11-28 12:10:20 +0000

    using util.promisify

--- 09-taskManager/services/taskdb.js
+++ 09-taskManager/services/taskdb.js
@@ -1,10 +1,28 @@
 //all persistence logic for tasks goes here
 var fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	util = require('util');
 
 var dbFile = path.join(__dirname, '../db/tasksdb.json');
 
-function readAll(){
+/*function promisify(callbackBasedApi){
+	return function(){
+		var args = arguments;
+		var promise = new Promise(function(resolveFn, rejectFn){
+			callbackBasedApi(...args, function(err, result){
+				if (err){
+					rejectFn(err);
+				} else {
+					resolveFn(result);
+				}
+			})
+		});
+		return promise;
+	}
+}*/
+
+/*function readAll(){
+
 	var promise = new Promise(function(resolveFn, rejectFn){
 		fs.readFile(dbFile, {encoding : 'utf8'}, function(err, fileContents){
 			if (err){
@@ -16,9 +34,17 @@ function readAll(){
 		})
 	});
 	return promise;
+}*/
+
+function readAll(){
+	var readFileAsync = util.promisify(fs.readFile);
+	return readFileAsync(dbFile, {encoding : 'utf8'})
+		.then(function(fileContents){
+			return JSON.parse(fileContents);
+		});
 }
 
-function save(tasks){
+/*function save(tasks){
 	var promise = new Promise(function(resolveFn, rejectFn){
 		fs.writeFile(dbFile, JSON.stringify(tasks), function(err){
 			if (err){
@@ -29,6 +55,11 @@ function save(tasks){
 		})	
 	});
 	return promise;
+}*/
+
+function save(tasks){
+	var writeFileAsync = util.promisify(fs.writeFile);
+	return writeFileAsync(dbFile, JSON.stringify(tasks));
 }
 
 module.exports = {
